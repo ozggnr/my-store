a8083fe55e2f8c830079f15bda866f0656934b15
ozggnr <ozgecaylioglu@gmail.com>
2022-04-18 14:14:56 -0400

    added about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotosComponent } from './photos/photos.component';
+import { AboutComponent } from './about/about.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent,
+    AboutComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
