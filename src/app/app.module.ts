54a41fb0140dfc7286358ca7589f078ba1a73daf
ozggnr <ozgecaylioglu@gmail.com>
2022-04-20 22:27:29 -0400

    added post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotosComponent } from './photos/photos.component';
-import { AboutComponent } from './about/about.component';
-import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotosComponent,
-    AboutComponent,
-    MovieListComponent
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
