877395b711833af79d45e5e0efdb421a7c7582c0
ozggnr <ozgecaylioglu@gmail.com>
2022-05-09 23:50:54 -0400

    added products component with some styling

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  products: Product[] = []
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(res => {
+      this.products = res
+    })
+  }
+
+}
