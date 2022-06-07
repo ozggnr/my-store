4a19d70230af7c723bde2857c5c02cca6e162e35
ozggnr <ozgecaylioglu@gmail.com>
2022-06-07 17:21:23 -0400

    added user service

---
+++ src/app/models/User.ts
@@ -0,0 +1,15 @@
+export class User {
+	id: number
+	userName: string
+	address: string
+	cardNumber: string
+	totalAmount: number
+
+	constructor() {
+		this.id = 1
+		this.userName = ''
+		this.address = ''
+		this.cardNumber = ''
+		this.totalAmount = 0
+	}
+}
