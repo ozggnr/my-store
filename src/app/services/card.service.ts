2864b7ca991b216d0a156957dc8575153bff7a47
ozggnr <ozgecaylioglu@gmail.com>
2022-06-07 17:23:13 -0400

    created remove card

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -17,22 +17,50 @@ export class CardService {
 	}
 
 	addToCart(product: Product, productAmount: number) {
-		const item = this.updateCardItem(product, productAmount)
+		const existingItemIndex = this.cardIemList.findIndex(item =>  item.id === product.id) as number
+
+		if (existingItemIndex >= 0) {
+			this.cardIemList[existingItemIndex].quantity = productAmount
+			this.cardIemList[existingItemIndex].totalPrice = this.calculateProductPrice(product.price, productAmount)
+		}
 		//To deep clone the product object
-		this.cardIemList.push(JSON.parse(JSON.stringify(item)))
+		else {
+			this.cardItem.id = product.id
+			this.cardItem.product = product
+			this.cardItem.quantity = productAmount
+			this.cardItem.totalPrice = this.calculateProductPrice(product.price, productAmount)
+			this.cardIemList.push(JSON.parse(JSON.stringify(this.cardItem)))
+		}
 		return this.cardIemList
 	}
 	getCardList() {
 		return this.cardIemList
 	}
-	updateCardList(cardItem: CardItem) {
-		console.log(cardItem)
+	updateCardList(cardItem: CardItem, amount: number) {
+		const existingItemIndex = this.cardIemList.findIndex(item =>  item.id === cardItem.id)
+		this.cardIemList[existingItemIndex].quantity = amount
+		this.cardIemList[existingItemIndex].totalPrice = this.calculateProductPrice(cardItem.product.price, amount)
+		return this.cardIemList
 	}
-	updateCardItem(product: Product, productAmount: number) {
-		this.cardItem.id = product.id
-		this.cardItem.product = product
-		this.cardItem.quantity = productAmount
-		this.cardItem.totalPrice = productAmount * product.price
-		return this.cardItem
+
+	getTotalCardPrice() {
+		return this.cardIemList.reduce((total, cardItem) => {
+			const price = Number(cardItem.totalPrice)
+			total += Number(cardItem.totalPrice)
+			return total
+		}, 0)
+	}
+	calculateProductPrice(productPrice: number, productAmount: number) {
+		const total = (productPrice * productAmount).toFixed(2) as unknown as number
+		console.log(total)
+		return total
+	}
+	removeFromCard(cardItem: CardItem) {
+		const newList = this.cardIemList.filter(item => item.id !== cardItem.id)
+		this.cardIemList = newList
+		console.log(this.cardIemList)
+		return this.cardIemList
 	}
 }
+
+
